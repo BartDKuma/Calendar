4bea46aadc121e099c36dec59cf0ded1ea6ea0a6
BartDKuma <vakuertz@gmail.com>
2025-12-01 21:36:37 -0500

    attempted link modification

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './scripts/index.js',
   output: {
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, './'),
     filename: 'index.js'
   },
   resolve: {
